c7b4e46dea0e1353b1769afa81a501e5d185a1b9
blyxyas <blyxyas@gmail.com>
2026-07-26 20:47:02 +0000

    wip

--- compiler/rustc_middle/src/query/calls.rs
+++ compiler/rustc_middle/src/query/calls.rs
@@ -128,7 +128,6 @@ fn try_get_cached<'tcx, C>(tcx: TyCtxt<'tcx>, cache: &C, key: C::Key) -> Option<
 where
     C: QueryCache,
 {
-    return None;
     match cache.lookup(&key) {
         Some((value, index)) => {
             tcx.prof.query_cache_hit(index.into());
@@ -151,7 +150,6 @@ pub(crate) fn query_get_at<'tcx, C>(
 where
     C: QueryCache,
 {
-    dbg!(query.name);
     match try_get_cached(tcx, &query.cache, key) {
         Some(value) => value,
         None => (query.execute_query_fn)(tcx, span, key, QueryMode::Get).unwrap(),
